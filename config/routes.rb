115e994af1a9aa22fd83ef55cb5c810570b025f4
Donny <donthephan@gmail.com>
2022-02-03 19:35:17 +0000

    add option to add new category

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  get 'about' => 'about#index'
+  # get 'about' => 'about#index'
+  resources :about, only: [:index]
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -17,6 +18,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, only: [:create, :new, :index]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
