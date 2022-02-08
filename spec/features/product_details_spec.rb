ac313fcb996ffd675124a6734d9526342039859c
Donny <donthephan@gmail.com>
2022-02-08 22:58:06 +0000

    product_details page navigation test complete

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
+RSpec.feature "Visitor Navigates to product details", type: :feature, js: true do
+  
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -17,13 +17,15 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "Navigate to an individual product" do
     # ACT
     visit root_path
-
+    p page.find('img.main-img')
+    page.first(:link, 'Details').click
+    
     # DEBUG / VERIFY
+    expect(page).to have_css 'section.products-show'
+    p page.find('img.main-img')
     save_screenshot
-    expect(page).to have_css 'article.product', count: 10
   end
-
 end
